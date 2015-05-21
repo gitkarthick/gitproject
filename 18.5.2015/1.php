7e1330b5fd494935a4f68504d39e4af6ab999593
gitkarthick <mayilai_karthik@yahoo.co.in>
2015-05-21 11:48:44 +0000

    Today commmit21.5.2015@5.20PM

--- 18.5.2015/1.php
+++ 18.5.2015/1.php
@@ -1,3 +1,8 @@
 <?php
 phpinfo();
+Process check command			
+# ps vax | sort -k8n			
+# top => press M			
+# ps -e -orss=,args= | sort -b -k1,1n			
+
 ?>
--- 19.5.2015/2.php
+++ 19.5.2015/2.php
@@ -1,3 +1,4 @@
-<?php
+i<?php
 phpinfo();
+Hi allllll
 ?>
