7c932247fd9e1a81da09ae1aeff7793c846ed6cf
gitkarthick <mayilai_karthik@yahoo.co.in>
2015-06-03 13:38:09 +0000

    Today commit - 3.6.2015 - Remove 5.php file

---
+++ 3.6.2015/2.php
@@ -0,0 +1,13 @@
+<?php
+phpinfo();
+echo "This is works fine"fvfwv
+Process check command			
+# ps vax | sort -k8n			
+# top => press M			
+# ps -e -orss=,args= | sort -b -k1,1n			
+Process check command			
+# ps vax | sort -k8n			
+# top => press M			
+# ps -e -orss=,args= | sort -b -k1,1n			
+
+?>
---
+++ 3.6.2015/4.php
@@ -0,0 +1,5 @@
+<?php
+echo "Welcome to phabricator Welcome!!"
+echo "This is works fine"
+
+?>
