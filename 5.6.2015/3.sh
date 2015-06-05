d7342e88f1a557da3a871d5c864879dc845cfc24
gitkarthick <mayilai_karthik@yahoo.co.in>
2015-06-05 10:32:51 +0000

    Today commits - 5.6.2015- Script files added

---
+++ 5.6.2015/3.sh
@@ -0,0 +1,14 @@
+# Check DNS
+nameservers=$(cat /etc/resolv.conf | sed '1 d' | awk '{print $2}')
+echo -e '\E[32m'"Name Servers :" $tecreset $nameservers 
+
+# Check Logged In Users
+who>/tmp/who
+echo -e '\E[32m'"Logged In users :" $tecreset && cat /tmp/who 
+
+# Check RAM and SWAP Usages
+free -m | grep -v + > /tmp/ramcache
+echo -e '\E[32m'"Ram Usages :" $tecreset
+cat /tmp/ramcache | grep -v "Swap"
+echo -e '\E[32m'"Swap Usages :" $tecreset
+cat /tmp/ramcache | grep -v "Mem"
---
+++ 5.6.2015/5.sh
@@ -0,0 +1,41 @@
+# Script for Monitor OS Type, OS Version, Architecture, Kernel Release, Hostname,Internal IP ,External IP ,Logged In users,Ram Usages ,Swap Usages,Disk Usages 
+#Load Average, System Uptime => DEBIAN/UBUNTU
+#! /bin/bash
+clear
+unset tecreset os architecture kernelrelease internalip externalip nameserver loadaverage
+while getopts iv name
+do
+        case $name in
+          i)iopt=1;;
+          v)vopt=1;;
+          *)echo "Invalid arg";;
+        esac
+done
+
+if [[ ! -z $iopt ]]
+then
+{
+wd=$(pwd)
+basename "$(test -L "$0" && readlink "$0" || echo "$0")" > /tmp/scriptname
+scriptname=$(echo -e -n $wd/ && cat /tmp/scriptname)
+su -c "cp $scriptname /usr/bin/monitor" root && echo "Congratulations! Script Installed, now run monitor Command" || echo "Installation failed"
+}
+fi
+
+if [[ ! -z $vopt ]]
+then
+{
+echo -e "Welcome"
+}
+fi
+
+if [[ $# -eq 0 ]]
+then
+{
+
+
+# Define Variable tecreset
+tecreset=$(tput sgr0)
+
+# Check if connected to Internet or not
+ping -c 1 google.com &> /dev/null && echo -e '\E[32m'"Internet: $tecreset Connected" || echo -e '\E[32m'"Internet: $tecreset Disconnected"
